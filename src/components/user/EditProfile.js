342c16f944d82b9f9ed345618a4633c43982f839
dattrungnam <e29bechanh@email.com>
2023-03-22 00:21:09 +0700

    Cập nhật thông tin tài khoản

--- src/components/user/EditProfile.js
+++ src/components/user/EditProfile.js
@@ -1,70 +1,156 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const EditProfile = () => {
+  const { id } = useParams();
+  const user = useSelector((state) => state.auth.login.currentUser);
+
+  const [inforUser, setInforUser] = useState({
+    username: "",
+    name: "",
+    phone: "",
+    address: "",
+    avatar: "",
+    newAvatar: "",
+  });
+  const [messageUpdate,setMessageUpdate] = useState("")
+  const [imgAvatar, setImgAvatar] = useState("")
+  const handleChange = (e) => {
+    setInforUser({
+      ...inforUser,
+      [e.target.name]: e.target.value,
+    });
+    setMessageUpdate("")
+  };
+  const fetchData = async (id) => {
+    const { data } = await axios.get(
+      `http://localhost:8080/v1/account/infor/${id}`,
+      {
+        headers: {
+          token: user.token,
+        },
+      }
+    );
   
+    return data;
+  };
+  const handleSubmit  = async (e) => {
+    e.preventDefault();
+ 
+
+    await fetch(`http://localhost:8080/v1/account/infor/${id}`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "token":`${user.token}`
+      },
+      body: JSON.stringify(inforUser),
+    }).then(res => res.json())
+    .then(res => setMessageUpdate(res.message))
+    
+  }
+  useEffect(() => {
+    fetchData(id).then((res) => {setInforUser(res)
+      setImgAvatar(res.avatar)
+    });
+
+
+    
+    
+  }, [id]);
   return (
     <>
       <section className="pt-[62px] min-h-screen flex flex-col items-center justify-center bg-gray-200  ">
         <div className="text-2xl my-6 ">Chỉnh sửa thông tin</div>
-        <div className="flex md:min-w-[720px]  px-8 py-6 gap-16 max-md:gap-8 max-md:px-4 h-full bg-white items-cente max-sm:w-full max-sm:flex-col justify-between rounded-lg border">
-          <div className="flex justify-center items-center">
-            <img
-              className="w-[160px] h-[160px] border-2 border-sky-200 object-cover rounded-full"
-              src="https://previews.dropbox.com/p/thumb/AB00JfIwDM0Yme3Fm9HFfT-c0o5NveVZA_9Boa9eH2iPyeNBXo57FHdZ7LXhuBVvtZTjnbozS7rOWm2WxnHSU7MgB89JDzQ-fwyuRWSrRlkIacuL-Vb9lyGgvCV6uBpKDw0jT8QSoRiz4OafcrLlTVBIMq5NaUReCeCjIHmIRrqZ9BE8azcbe1otH46y3dgd6t4NGc5AM3Rxc3AotL1P8qlY5l9At4QCVt05xcEYd2I9s1NKzWfYHnvvL9sFlGjKM-_PHa6T4n5LlL0g6ju1sZv9frT9oQ-z9GF1UnxuINQCEaiH8XubqeeU1GVky9AAMX_R7xDnZiBlb3HCgmBzyEifF5bjfZ0-gv0jaw0_IMTw2w9syMlRTP-qI7U62aSptvU/p.jpeg"
-              alt=""
-            />
+        <form onSubmit={handleSubmit}>
+          <div className="flex md:min-w-[720px] min-h-[440px] px-8 py-6 gap-16 max-md:gap-8 max-md:px-4 h-full bg-white items-cente max-sm:w-full max-sm:flex-col justify-between rounded-lg border">
+            <div className="flex justify-center items-center">
+              {imgAvatar ? (
+                <img
+                  className="w-[160px] h-[160px] border-2 border-sky-200 object-cover rounded-full"
+                  src={imgAvatar}
+                  alt=""
+                />
+              ) : (
+                <img
+                  className="w-[160px] h-[160px] border-2 border-sky-200 object-cover rounded-full"
+                  src={
+                    "https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg"
+                  }
+                  alt=""
+                />
+              )}
+            </div>
+            <div className=" flex flex-col py-6 px-4">
+              <div className=" flex items-center justify-center">
+                <div className="min-w-[160px] max-md:min-w-[140px] font-medium ">
+                  Tên đăng nhập:
+                </div>
+                <input
+                  className=" py-2 pl-8 pr-8 mb-2  bg-gray-200   rounded-2xl ring-1 outline-none "
+                  readOnly
+                  value={inforUser?.username}
+                  name="username"
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="min-w-[160px] max-md:min-w-[140px] font-medium">
+                  Họ tên:
+                </div>
+                <input
+                  className=" py-2 pl-8 pr-8 mb-2 bg-white  rounded-2xl ring-1 outline-blue-500"
+                  name="name"
+                  value={inforUser?.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="min-w-[160px] max-md:min-w-[140px]  font-medium">
+                  Số điện thoại:
+                </div>
+                <input
+                  className=" py-2 pl-8 pr-8 mb-2 bg-white  rounded-2xl ring-1 outline-blue-500"
+                  name="phone"
+                  value={inforUser?.phone}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="min-w-[160px] max-md:min-w-[140px] font-medium">
+                  Địa chỉ:
+                </div>
+                <input
+                  className=" py-2 pl-8 pr-8 mb-2 bg-white  rounded-2xl ring-1 outline-blue-500"
+                  name="address"
+                  value={inforUser?.address}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="min-w-[160px] max-md:min-w-[140px] font-medium">
+                  Avatar:
+                </div>
+                <input
+                  className=" py-2 pl-8 pr-8 mb-2 bg-white  rounded-2xl ring-1 outline-blue-500"
+                  name="avatar"
+                  value={inforUser?.avatar}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center mt-4">
+                {messageUpdate ? <p className="mb-4 text-[green]">{messageUpdate}</p> : <></>}
+                <button
+                  type="submit"
+                  class=" py-1 bg-[#194284] w-[180px] rounded-2xl text-blue-50 text-[20px] font-semibold hover:opacity-75"
+                >
+                  Lưu
+                </button>
+              </div>
+            </div>
           </div>
-          <div className=" flex flex-col py-6 px-4">
-            <div className=" flex items-center justify-center">
-              <div className="min-w-[160px] max-md:min-w-[140px] font-medium ">Tên đăng nhập:</div>
-              <input
-                className=" py-2 pl-8 pr-8 mb-2  bg-gray-200   rounded-2xl ring-1 outline-none "
-                readOnly
-                value={"abbb"}
-              />
-            </div>
-            <div className="flex items-center justify-center">
-              <div className="min-w-[160px] max-md:min-w-[140px] font-medium">Họ tên:</div>
-              <input
-                className=" py-2 pl-8 pr-8 mb-2 bg-white  rounded-2xl ring-1 outline-blue-500"
-                
-                
-              />
-            </div>
-            <div className="flex items-center justify-center">
-              <div className="min-w-[160px] max-md:min-w-[140px]  font-medium">Số điện thoại:</div>
-              <input
-                className=" py-2 pl-8 pr-8 mb-2 bg-white  rounded-2xl ring-1 outline-blue-500"
-                
-                
-              />
-            </div>
-            <div className="flex items-center justify-center">
-              <div className="min-w-[160px] max-md:min-w-[140px] font-medium">Địa chỉ:</div>
-              <input
-                className=" py-2 pl-8 pr-8 mb-2 bg-white  rounded-2xl ring-1 outline-blue-500"
-                
-                
-              />
-            </div>
-            <div className="flex items-center justify-center">
-              <div className="min-w-[160px] max-md:min-w-[140px] font-medium">Avatar:</div>
-              <input
-                className=" py-2 pl-8 pr-8 mb-2 bg-white  rounded-2xl ring-1 outline-blue-500"
-                
-                
-              />
-            </div>
-            <div className="flex items-center justify-center mt-4">
-              <button
-                type="submit"
-                class=" py-1 bg-[#194284] w-[180px] rounded-2xl text-blue-50 text-[20px] font-semibold hover:opacity-75"
-              >
-                Lưu
-              </button>
-            </div>
-          </div>
-        </div>
+        </form>
       </section>
     </>
   );
